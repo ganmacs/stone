9ba6076e81c9909750cda322f39b1a89a3e18e11
ganmacs <ganmacs@gmail.com>
2015-04-25 09:44:58 +0900

    fix element require

--- lib/stone/parser/combinator.rb
+++ lib/stone/parser/combinator.rb
@@ -1,13 +1,5 @@
-require 'stone/parser/elements/id'
-require 'stone/parser/elements/tree'
-require 'stone/parser/elements/number'
-require 'stone/parser/elements/skip'
-require 'stone/parser/elements/string'
-require 'stone/parser/elements/number'
-require 'stone/parser/elements/repeat'
-require 'stone/parser/elements/or_tree'
-require 'stone/parser/elements/expression'
 require 'stone/parser/ast_builder'
+require 'stone/parser/element'
 
 module Stone
   module Parser
---
+++ lib/stone/parser/element.rb
@@ -0,0 +1,15 @@
+require 'stone/parser/elements/expression'
+require 'stone/parser/elements/id'
+require 'stone/parser/elements/number'
+require 'stone/parser/elements/or_tree'
+require 'stone/parser/elements/repeat'
+require 'stone/parser/elements/skip'
+require 'stone/parser/elements/string'
+require 'stone/parser/elements/tree'
+
+module Stone
+  module Parser
+    module Element
+    end
+  end
+end
