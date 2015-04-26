717ec86d831cbce648b20e1e6cbd210fbd88cea3
ganmacs <ganmacs@gmail.com>
2015-04-26 19:43:24 +0900

    fix fixrture file

--- spec/stone/lexer_spec.rb
+++ spec/stone/lexer_spec.rb
@@ -2,25 +2,30 @@ require 'stone/lexer'
 require 'stone/line_number_reader'
 
 describe Stone::Lexer do
-  let(:file) { 'examples/sample' }
+  let(:file) { 'spec/examples/while' }
   let(:reader) { Stone::LineNumberReader.new(file) }
   let(:lexer) { described_class.new(reader) }
 
   describe 'do lexical analysing' do
     it '#read_token read token one by one disruptive' do
+      expect(lexer.read_token.text).to eq 'i'
+      expect(lexer.read_token.text).to eq '='
+      expect(lexer.read_token.text).to eq '0'
+      expect(lexer.read_token.text).to eq "\n"
+      expect(lexer.read_token.text).to eq 'sum'
+      expect(lexer.read_token.text).to eq '='
+      expect(lexer.read_token.text).to eq '0'
+      expect(lexer.read_token.text).to eq "\n"
       expect(lexer.read_token.text).to eq 'while'
       expect(lexer.read_token.text).to eq 'i'
       expect(lexer.read_token.text).to eq '<'
-      expect(lexer.read_token.text).to eq '10'
-      expect(lexer.read_token.text).to eq '{'
-      expect(lexer.read_token.text).to eq "\n"
     end
 
     it '#peek_token peek token one by one' do
-      expect(lexer.peek_token.text).to eq 'while'
-      expect(lexer.peek_token.text).to eq 'while'
-      expect(lexer.read_token.text).to eq 'while'
       expect(lexer.peek_token.text).to eq 'i'
+      expect(lexer.peek_token.text).to eq 'i'
+      expect(lexer.read_token.text).to eq 'i'
+      expect(lexer.peek_token.text).to eq '='
     end
   end
 end
